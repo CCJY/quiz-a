10e5753b03286c99f9f8ba1cda4a568906195cbd
Junyoung Choi <ccjy1113@gmail.com>
2021-02-13 22:53:42 -0500

    parameter type checking

--- quiz/export_json.py
+++ quiz/export_json.py
@@ -1,7 +1,7 @@
 import csv
 
 
-def jsonToCsv(datas, filename='offers.csv'):
+def jsonToCsv(datas: list, filename: str = 'offers.csv'):
     with open(filename, 'w', encoding="utf-8", newline="") as f:
         csv_writer = csv.writer(f)
         for row in datas:
